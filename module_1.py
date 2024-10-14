72d99fcf3366a901095359b26ed6b86525c2dba3
ElenMartyn <elenamartyn00@gmail.com>
2024-10-14 22:12:27 +0700

    Update module_1.py

--- module_1.py
+++ module_1.py
@@ -1,7 +1,6 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = sorted(students)
-
 GPA = {
     students[0]: sum(grades[0]) / len(grades[0]),
     students[1]: sum(grades[1]) / len(grades[1]),
@@ -9,6 +8,5 @@ GPA = {
     students[3]: sum(grades[3]) / len(grades[3]),
     students[4]: sum(grades[4]) / len(grades[4])
 }
-
 print(GPA)
 
